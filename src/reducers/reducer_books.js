4bab247051aee2f170a21380a5372dbc4df0ba8e
Stephen Gadd <stephen.gadd@pdri.com>
2017-09-05 14:12:27 -0400

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return [
+        {title: "book-1", pages: 101},
+        {title: "book-2", pages: 234},
+        {title: "book-3", pages: 12},
+        {title: "book-4", pages: 52}
+    ]
+}
